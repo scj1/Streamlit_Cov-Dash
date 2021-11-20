f3906d66bec39f86f1fe866e2a94f041f55645a1
scj1 <94457724+scj1@users.noreply.github.com>
2021-11-20 16:36:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,253 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from libraries import *
+from streamlit_player import st_player
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.set_page_config(page_title='Covid Dashboard', page_icon="💉")
+
+
+countries = ['Australia', 'China','India','Italy', 'Japan', 'USA', 'UK', 'Sri Lanka', 'Spain', 'Brazil']
+country_code = {'Australia': 'au', 'China': 'cn', 'India': 'in', 'Italy':'it', 'Japan': 'jp', 'USA': 'us', 'UK': 'gb', 'Sri Lanka': 'lk', 'Spain': 'es', 'Brazil':'br'}
+
+data_types = ['cases', 'deaths', 'recovered']
+
+country = st.sidebar.selectbox('Pick a country', countries)
+
+
+
+days = st.sidebar.slider('Pick your days', min_value=1,max_value=90)
+
+
+data_type = st.sidebar.multiselect('Pick data types', data_types)
+
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+
+
+with st.sidebar.expander("About"):
+         st.write( "Developed by Dinuja de Silva")
+
+with st.sidebar.expander("Version"):
+    st.write("1.0.1")
+
+
+
+
+
+
+
+cases_df = get_historic_cases(country,days)
+
+deaths_df = get_historic_deaths(country,days)
+
+recoveries_df = get_historic_recoveries(country,days)
+
+historic_df = pd.concat([cases_df,deaths_df,recoveries_df],axis=1).astype(int)
+
+daily_cases_df = get_daily_cases(country,days)
+
+daily_deaths_df = get_daily_deaths(country,days)
+
+daily_recoveries_df = get_daily_recoveries(country,days)
+
+daily_df = pd.concat([daily_cases_df,daily_deaths_df,daily_recoveries_df],axis=1).astype(int)
+
+yesterday_cases = get_yesterday_cases(country)
+
+yesterday_deaths = get_yesterday_deaths(country)
+
+yesterday_recoveries = get_yesterday_recoveries(country)
+
+st.title('Covid Dashboard')
+
+st.metric('Country', country)
+
+st.image(f"https://flagcdn.com/80x60/{country_code[country]}.png")
+
+st.metric('Total Cases', yesterday_cases)
+
+st.metric('Total Deaths', yesterday_deaths)
+
+st.metric('Total recovered', yesterday_recoveries)
+
+st.bar_chart(daily_df[data_type])
+st.balloons()
+st_player("https://www.youtube.com/watch?v=oqFn6AHoJZQ")
+
+
+lib_code_contents = '''
+import requests
+import pandas as pd
+
+def get_historic_cases(country, days):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+  data = resp.json()
+  cases_df = pd.DataFrame(data=data['timeline']['cases'].values(), 
+                    index=data['timeline']['cases'].keys(),
+                    columns=['cases'])
+
+  return cases_df
+
+def get_historic_deaths(country, days):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+  data = resp.json()
+  deaths_df = pd.DataFrame(data=data['timeline']['deaths'].values(), 
+                    index=data['timeline']['deaths'].keys(),
+                    columns=['deaths'])
+
+  return deaths_df
+
+def get_historic_recoveries(country, days):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+  data = resp.json()
+  recoveries_df = pd.DataFrame(data=data['timeline']['recovered'].values(), 
+                    index=data['timeline']['recovered'].keys(),
+                    columns=['recovered'])
+
+  return recoveries_df
+
+def get_yesterday_cases(country):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
+  data = resp.json()
+  count = list(data['timeline']['cases'].values())[0]
+
+  return count 
+
+def get_yesterday_deaths(country):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
+  data = resp.json()
+  count = list(data['timeline']['deaths'].values())[0]
+
+  return count 
+
+def get_yesterday_recoveries(country):
+
+  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
+  data = resp.json()
+  count = list(data['timeline']['recovered'].values())[0]
+
+  return count 
+
+def get_daily_cases(country,days):
+
+    resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+    data = resp.json()
+    cumilative = pd.DataFrame(data=data['timeline']['cases'].values(), 
+                    index=data['timeline']['cases'].keys(),
+                    columns=['cases'])
+    daily = cumilative.diff().fillna(0)
+    return daily 
+    
+def get_daily_deaths(country,days):
+
+    resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+    data = resp.json()
+    cumilative = pd.DataFrame(data=data['timeline']['deaths'].values(), 
+                    index=data['timeline']['deaths'].keys(),
+                    columns=['deaths'])
+    daily = cumilative.diff().fillna(0)
+    return daily 
+
+def get_daily_recoveries(country,days):
+
+    resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
+    data = resp.json()
+    cumilative = pd.DataFrame(data=data['timeline']['recovered'].values(), 
+                    index=data['timeline']['recovered'].keys(),
+                    columns=['recovered'])
+    daily = cumilative.diff().fillna(0)
+    return daily 
+
+'''
+st.download_button('Tap to download the library code', lib_code_contents)
+
+application_code_contents = '''
+import pandas as pd
+import streamlit as st
+from libraries import *
+
+
+
+
+st.set_page_config(page_title='Covid Dashboard', page_icon="💉")
+
+
+countries = ['Australia', 'China','India','Italy', 'Japan', 'USA', 'UK', 'Sri Lanka', 'Spain', 'Brazil']
+
+
+data_types = ['cases', 'deaths', 'recovered']
+
+country = st.sidebar.selectbox('Pick a country', countries)
+
+
+
+days = st.sidebar.slider('Pick your days', min_value=1,max_value=90)
+
+
+data_type = st.sidebar.multiselect('Pick data types', data_types)
+
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+st.sidebar.write("  ")
+
+
+with st.sidebar.expander("About"):
+         st.write( "Developed by Casual Coders")
+
+with st.sidebar.expander("Version"):
+    st.write("1.0.1")
+
+
+cases_df = get_historic_cases(country,days)
+
+deaths_df = get_historic_deaths(country,days)
+
+recoveries_df = get_historic_recoveries(country,days)
+
+historic_df = pd.concat([cases_df,deaths_df,recoveries_df],axis=1).astype(int)
+
+daily_cases_df = get_daily_cases(country,days)
+
+daily_deaths_df = get_daily_deaths(country,days)
+
+daily_recoveries_df = get_daily_recoveries(country,days)
+
+daily_df = pd.concat([daily_cases_df,daily_deaths_df,daily_recoveries_df],axis=1).astype(int)
+
+yesterday_cases = get_yesterday_cases(country)
+
+yesterday_deaths = get_yesterday_deaths(country)
+
+yesterday_recoveries = get_yesterday_recoveries(country)
+
+st.title('Covid Dashboard')
+
+st.metric('Country', country)
+
+st.metric('Total Cases', yesterday_cases)
+
+st.metric('Total Deaths', yesterday_deaths)
+
+st.metric('Total recovered', yesterday_recoveries)
+
+st.area_chart(daily_df[data_type])
+'''
+st.download_button('Tap to download the application code', application_code_contents)
+
